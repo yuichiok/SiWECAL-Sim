8b6a774155f4021755a8c40a94c16fd722d2e292
Fabricio Jimenez <fabricioajm@gmail.com>
2022-03-11 11:57:09 +0100

    Have generic write_geom_desc and confs for all years geometries

---
+++ generation/confs.py
@@ -0,0 +1,65 @@
+W_Confs = {
+    "TB2021-11_CONF0" : [ [0, True, 320], # no absorber
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 500], # 0.5mm wafer
+                          [0, True, 500], # 0.5mm wafer
+                          [0, True, 500], # 0.5mm wafer
+                          [0, True, 500], # 0.5mm wafer
+                          [0, True, 320],
+                          [0, True, 320], # Careful, wf1 0.5
+                          [0, True, 320],
+                          [0, True, 320] ],
+    
+    "TB2021-11_CONF1" : [ [0, True, 320], # no absorber
+                          [3, True, 320],
+                          [3, True, 320],
+                          [3, True, 320],
+                          [3, True, 320],
+                          [3, True, 320],
+                          [3, True, 320],
+                          [3, True, 500], # 0.5mm wafer
+                          [3, True, 500], # 0.5mm wafer
+                          [3, True, 500], # 0.5mm wafer
+                          [3, True, 500], # 0.5mm wafer
+                          [3, True, 320],
+                          [6, True, 320], # Careful, wf1 0.5
+                          [6, True, 320],
+                          [6, True, 320] ],
+    
+    "TB2022-03_CONF0" : [ [0, True, 650],
+                          [0, True, 650],
+                          [0, True, 500],
+                          [0, True, 500],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 500],
+                          [0, True, 500],
+                          [0, True, 500],
+                          [0, True, 500],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320],
+                          [0, True, 320] ],
+    
+    "TB2022-03_CONF1" : [ [4, True, 650],
+                          [4, True, 650],
+                          [4, True, 500],
+                          [4, True, 500],
+                          [4, True, 320],
+                          [4, True, 320],
+                          [4, True, 320],
+                          [6, True, 500],
+                          [6, True, 500],
+                          [6, True, 500],
+                          [6, True, 500],
+                          [6, True, 320],
+                          [6, True, 320],
+                          [6, True, 320],
+                          [6, True, 320] ],
+    }
