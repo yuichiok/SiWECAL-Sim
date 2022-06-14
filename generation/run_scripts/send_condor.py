37991333ef8bf28fbe85660e186dc79741fcb7e4
Fabricio Jimenez <fabricioajm@gmail.com>
2022-06-14 18:13:40 +0200

    Added initial index and mkdirs in send_condor

--- generation/run_scripts/send_condor.py
+++ generation/run_scripts/send_condor.py
@@ -13,15 +13,11 @@ parser.add_argument('--beam_x', default="0", type=str, help="Beam x position (de
 parser.add_argument('--beam_y', default="0", type=str, help="Beam y position (deafult 0)")
 parser.add_argument('--angle', default=0, help="Incidence angle of beam (testing this feature).")
 parser.add_argument('--nevents', default=100, type=int, help="Number of events (default 100).")
-parser.add_argument('--events_per_job', default=5000, type=int, help="Max number of events per batch job (default 5000).")
+parser.add_argument('--events_per_job', default=1000, type=int, help="Max number of events per batch job (default 1000).")
+parser.add_argument('--first_job_index', default=1, type=int, help="Index of the first job/filename (default 1).")
 parser.add_argument('--tbconf', default='TB2022-03_CONF0', help="TB+Conf as defined in confs.py (default TB2022-03_CONF0).")
-parser.add_argument('--test_run', action="store_true", default=False, help="Send test run to condor.")
+# parser.add_argument('--test_run', action="store_true", default=False, help="Send test run to condor.")
 parser.add_argument('--run_locally', action="store_true", help="Run locally and not in Condor.")
-
-# def mkdirs():
-#     # If not dirs steer data log macros, create them
-#     # Likely not needed
-#     pass
 
 class Batch:
     def __init__(self,
@@ -30,23 +26,25 @@ class Batch:
                  beam_x,
                  beam_y,
                  angle,
-                 total_n_events,
+                 nevents,
                  events_per_job,
+                 first_job_index,
                  physics_list,
                  tbconf,
                  data_path,
                  ilcsoft_path,
                  geometry_folder,
-                 submit_command):
+                 submit_command,
+                 run_locally): # run_locally is not used in the class
 
         self.particle = particle
         self.energy = energy
         self.beam_x = beam_x
         self.beam_y = beam_y
         self.angle = angle
-        self.total_n_events = total_n_events 
+        self.nevents = nevents 
         self.events_per_job = events_per_job 
-        if total_n_events < events_per_job: self.events_per_job = total_n_events
+        if nevents < events_per_job: self.events_per_job = nevents
         self.physics_list = physics_list
         self.tbconf = tbconf
         self.tb, self.conf = tbconf.split("_") 
@@ -58,24 +56,27 @@ class Batch:
         self.geometry_folder = geometry_folder
         self.ilcsoft_path = ilcsoft_path
         self.submit_command = submit_command
-        self.jobs_events = np.full(int(total_n_events / events_per_job)+1, events_per_job)
-        if (total_n_events % events_per_job != 0):
+        self.jobs_events = np.full(int(nevents / events_per_job)+1, events_per_job)
+        if (nevents % events_per_job != 0):
             # rounding issue here can give 1 less event but...
-            self.jobs_events[-1] = int(events_per_job * ((total_n_events / events_per_job) % 1))
+            self.jobs_events[-1] = int(events_per_job * ((nevents / events_per_job) % 1))
         else:
             self.jobs_events = np.delete(self.jobs_events, -1)
         print("Launching", len(self.jobs_events), "jobs.\n", self.jobs_events)
+        self.first_job_index = first_job_index
         # TODO: 
         #- Include mkdir CONF{N}/{build,lcio}
         # init counter of batch jobs
 
 
     def mkdirs(self):
-        pass
+        os.mkdirs(self.data_path)
+        os.mkdirs(self.data_path[:-5] + "build")
 
     def write_g4macro(self):
         for it, nev in enumerate(self.jobs_events): 
-            macroname = self.g4macroname[:-4] + "_" + str(it+1) + ".mac"
+            ijob = it + self.first_job_index 
+            macroname = self.g4macroname[:-4] + "_" + str(ijob) + ".mac"
             with open(macroname, 'w') as f:
                 f.write("/gps/verbose 1\n")
                 f.write("/gps/particle {}\n".format(self.particle))
@@ -95,19 +96,19 @@ class Batch:
 
     
     def write_pyscripts(self):
-        #for it in range(1, int(self.total_n_events/self.events_per_job)+1):
         for it, _ in enumerate(self.jobs_events):
+            ijob = it + self.first_job_index
             label = "{}_{}_{}_{}GeV_{}".format(self.physics_list,
              self.tbconf,
              self.particle,
              self.energy,
-             it+1)  
+             ijob)  
              # Should be only one label...
             label2 = "ECAL_{}_conf{}_{}_{}GeV_{}".format(self.physics_list,
                                                          self.conf[-1], ## Sould be debugged!!!
                                                          self.particle,
                                                          self.energy,
-                                                         it+1)
+                                                         ijob)
             print("Label is", label)
             pyscript = self.steer_path + "/runddsim_" + label + ".py"
             with open(pyscript, 'w') as f:
@@ -136,14 +137,15 @@ class Batch:
     def write_shcondors(self):
         #for it in range(1, 21):
         for it, _ in enumerate(self.jobs_events):
+            ijob = it + self.first_job_index
             label = "{}_{}_{}_{}GeV_{}".format(self.physics_list,
              self.tbconf,
              self.particle,
              self.energy,
-             it+1)  
+             ijob)  
             pyscript = self.steer_path + "runddsim_" + label + ".py"
             condorsh = self.steer_path + "/runddsim_" + label + ".sh"
-            macroname = self.g4macroname[:-4] + "_" + str(it+1) + ".mac"
+            macroname = self.g4macroname[:-4] + "_" + str(ijob) + ".mac"
             with open(condorsh, 'w') as f:
                 f.write("source {}/init_ilcsoft.sh\n".format(self.ilcsoft_path)) 
                 print(os.getcwd())
@@ -153,16 +155,15 @@ class Batch:
                 f.write("ddsim --enableG4GPS --macroFile {}/{} --steeringFile {}\n".format(os.getcwd(), macroname, pyscript))
             os.chmod(condorsh, 0o755)
             print("Condor script written in", condorsh)
-            # break
 
     def submit_jobs(self):
-        #for it in range(1, 21):
         for it, _ in enumerate(self.jobs_events):
+            ijob = it + self.first_job_index
             label = "{}_{}_{}_{}GeV_{}".format(self.physics_list,
              self.tbconf,
              self.particle,
              self.energy,
-             it+1)
+             ijob)
             #condorsh = self.steer_path + "/runddsim_" + label + ".sh"
             condorsh = "runddsim_" + label + ".sh"
             wd = os.getcwd()
@@ -180,16 +181,19 @@ if __name__ == "__main__":
     with open("condor_config.yml", "r") as ymlfile:
         config = yaml.safe_load(ymlfile)
     
-    batch = Batch(particle = args.particle,
-                  energy = args.energy,
-                  beam_x = args.beam_x,
-                  beam_y = args.beam_y,
-                  angle = 0,
-                  total_n_events = args.nevents,
-                  events_per_job = args.events_per_job,
-                  tbconf = args.tbconf,
-                  **config)
+    # batch = Batch(particle = args.particle,
+    #               energy = args.energy,
+    #               beam_x = args.beam_x,
+    #               beam_y = args.beam_y,
+    #               angle = 0,
+    #               nevents = args.nevents,
+    #               events_per_job = args.events_per_job,
+    #               first_job_index = args.first_job_index,
+    #               tbconf = args.tbconf,
+    #               **config)
+    batch = Batch(**vars(args), **config)
     print(batch.steer_path)
+    batch.mkdirs()
     batch.write_g4macro()
     batch.write_pyscripts()
     batch.write_shcondors()
